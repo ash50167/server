b14a35ba3f4ec64f2afee28cd1f04cd2b97e785b
Akash <realakash39@gmail.com>
2025-04-03 18:02:54 +0530

    commit_4

--- server.js
+++ server.js
@@ -10,7 +10,17 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE", allowedHeaders: ["Content-Type", "Authorization"], credentials: true }));
+// app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE", allowedHeaders: ["Content-Type", "Authorization"], credentials: true }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://client-0eev.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 app.use(cookieParser());
 
 
