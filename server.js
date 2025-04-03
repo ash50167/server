6b3b35a07eb2bc2af7f6708eb8dd4c607c275d0a
Akash <realakash39@gmail.com>
2025-04-03 17:49:00 +0530

    commit_3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "https://client-0eev.onrender.com", methods: "GET,POST,PUT,DELETE", allowedHeaders: ["Content-Type", "Authorization"], credentials: true }));
+app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE", allowedHeaders: ["Content-Type", "Authorization"], credentials: true }));
 app.use(cookieParser());
 
 
